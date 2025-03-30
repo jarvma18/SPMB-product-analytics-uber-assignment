7a9e62559e6bfcee6e8c3ec92d2bc6579764dab9
Markus Järvinen <markusmjarvinen@outlook.com>
2025-03-30 21:07:39 +0000

    Added ttest function.

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from lib.csv_import import read_csv_file
 import pandas as pd
 from scipy.stats import ttest_ind
 
-def main():
-  df = read_csv_file("data/uber_dataset.csv")
+def run_t_test(first_sample: pd.Series, first_sample_title: str, second_sample: pd.Series, second_sample_title: str):
+  t_stat, p_value = ttest_ind(first_sample, second_sample, equal_var=False)
+  print(f"Mean {first_sample_title}: {first_sample.mean()}, Mean {second_sample_title}: {second_sample.mean()}")
+  print(f"t = {t_stat:.3f}, p = {p_value:.3f}")
+  print("Statistically significant at 5%" if p_value < 0.05 else "Not statistically significant")
 
 main()
