8a1bfaab54102b268c709d78c73c6ead6a8bd8d9
Markus Järvinen <markusmjarvinen@outlook.com>
2025-03-31 21:13:39 +0000

    Ttest prints now 6 decimal and added optional dollar sign to print labeled amount to have it usable in some other use cases.

--- lib/shared.py
+++ lib/shared.py
@@ -4,7 +4,7 @@ from scipy.stats import ttest_ind
 def run_t_test(first_sample: pd.Series, first_sample_title: str, second_sample: pd.Series, second_sample_title: str):
   t_stat, p_value = ttest_ind(first_sample, second_sample, equal_var=False)
   print(f"Mean {first_sample_title}: {first_sample.mean()}, Mean {second_sample_title}: {second_sample.mean()}")
-  print(f"t = {t_stat:.3f}, p = {p_value:.3e}")
+  print(f"t = {t_stat:.3f}, p = {p_value:.6f}")
   print("Statistically significant at 5%\n" if p_value < 0.05 else "Not statistically significant\n")
 
 def print_difference(firstValue: float, otherValue: float):
@@ -18,5 +18,8 @@ def print_higher_number_choice(firstChoice: str, firstChoiceSum: float, otherCho
   print(firstChoice, ':', firstChoiceSum, 'vs', otherChoice, ':', otherChoiceSum)
   print(firstChoicePrint if firstChoiceHasHigherNumber else otherChoicePrint)
 
-def print_labeled_amount(title: str, value: float):
-  print(title, ':', value, '$')
+def print_labeled_amount(title: str, value: float, addDollar):
+  if addDollar:
+    print(title, ':', value, '$')
+  else:
+    print(title, ':', value)
